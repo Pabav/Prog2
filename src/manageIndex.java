f8c3e8c71576e82e83aabf41be81ea58c571d662
B <pba025@gmail.com>
2025-12-04 16:31:38 +0100

    recursive html creation no index

---
+++ src/manageIndex.java
@@ -0,0 +1,16 @@
+import java.io.File;
+
+public class manageIndex extends manageHTML
+{
+
+    public manageIndex(File path)
+    {
+        super(path);
+    }
+
+    @Override
+    public void create()
+    {
+        super.create();
+    }
+}
